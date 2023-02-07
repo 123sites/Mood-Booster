933647b20294c4e748fcf2f1a427b8931e2cd4ae
CF <cfreitas12345@gmail.com>
2023-02-06 18:05:46 -0700

    Added query selectors & innerHTML for joke & meme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,90 @@
-// KEEP BELOW, LINES 1-39 WORKS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
+// KEEP BELOW, LINES 1-40 WORKS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// Joke & meme buttons
 var jokeBtn = document.getElementById("jokeBtn");
-  document.addEventListener("click", choice);
+jokeBtn.addEventListener("click", jokeChoice);
 
-var memeBtn = document.getElementById("memeBtn")
-  document.addEventListener("click", choice);
+var memeBtn = document.getElementById("memeBtn");
+memeBtn.addEventListener("click", memeChoice);
 
-function choice() {
-
+function jokeChoice() {
   // let joke = `https://sv443.net/jokeapi/v2/?ref=apilist.fun#safe-mode`;
 
-// KEEP MEME BELOW, REDO JOKE API (Joke error on line ) >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//   let joke = `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`;
-//     fetch(joke, {
-//       params: {exclude: "nsfw, insults, jewish, religious, dark, racist, sexual, sexist,"}
-//   }).then(function (response) {
-//     console.log(response);
-//     return response.json()
-//   }).then(function(data){
-//     console.log(data);
-//   },)
-
-// let meme = `https://reddit-meme.p.rapidapi.com/memes/top`;
-// fetch(meme, {
-//       headers: {
-//         "x-rapidAPI-key": "e4424d5624mshff482b6755fbc8ep172344jsn21cfbb7115c2",
-//         "x-rapidAPI-host": "reddit-meme.p.rapidapi.com",
-
-//       },
-//   }).then(function (response) {
-//     console.log(response);
-//     return response.json()
-//   }).then(function(data){
-//     console.log(data);
-//   },)
+  let joke = `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`;
+  fetch(joke, {
+    params: {
+      exclude:
+        "nsfw, insults, jewish, religious, dark, racist, sexual, sexist,",
+    },
+  })
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      let h2=document.createElement("h2");
+      h2.textContent=data.joke;
+      console.log(data.joke);
+      document.querySelector("#display").innerHTML=data.joke;
+      // document.querySelector("#display").innerHTML="";
+      // document.querySelector("#display").append(h2);
+      document.querySelector(".getChoice").classList.add("hide");
+      document.querySelector("#results").classList.remove("hide");
+    });
 }
 
-choice()
+function memeChoice (){
+  let meme = `https://reddit-meme.p.rapidapi.com/memes/top`;
+  fetch(meme, {
+    headers: {
+      "x-rapidAPI-key": "e4424d5624mshff482b6755fbc8ep172344jsn21cfbb7115c2",
+      "x-rapidAPI-host": "reddit-meme.p.rapidapi.com",
+    },
+  })
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+// const fetchImage = async meme => {
+//   const response = await fetch(meme)
+//   const blob = await response.blob()
+
+//   return blob
+// },
+
+// const memeBtn = async meme => {
+//   const imageBlob = await fetchImage(meme)
+//   const imageBase64 = meme.createObjectURL(imageBlob)
+//   console.log({imageBase64})
+
+//   const a = document.createElement('a')
+//   a.style.setProperty('display', 'none')
+//   document.body.appendChild(a)
+//   a.download = meme.replace(/^.*[\\\/]/, '')
+//   a.href = imageBase64
+//   a.click()
+//   a.remove()
+// }
+// },
+
+// function show() {
+
+//     /* Access image by id and change
+//     the display property to block*/
+//     document.getElementById('image')
+//             .style.display = "block";
+
+//     document.getElementById('memeBtn')
+//             .style.display = "none";
+// }
+
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-  // <input type="submit" id="btn" value="Generate Jokes" />
-  // <input type="submit" id="btn" value="Generate Memes" />
+// <input type="submit" id="btn" value="Generate Memes" />
 
 // $().append(choice);
 
@@ -68,8 +112,6 @@ choice()
 //   }
 // });
 // })})}
-
-
 
 // END>>>>>>>>>>> KEEP, MODIFY, OR DELETE ??????????????
 // var badRequestUrl = 'https://api.github.com/unicorns';
@@ -101,29 +143,29 @@ choice()
 // loadUp();
 
 // PREVIOUS JOKE, DELETE BELOW ??????
-  // jokeApiKey = "IcgIu9A1Tg9S";
-  // https://jokes.one/api/joke/
-  // let joke = `https//api......q=${}....${jokeApiKey}`;
+// jokeApiKey = "IcgIu9A1Tg9S";
+// https://jokes.one/api/joke/
+// let joke = `https//api......q=${}....${jokeApiKey}`;
 
-  //  (has memes and jokes)    url: 'https://random-stuff-api.p.rapidapi.com/joke',
-  // let joke = `https://random-stuff-api.p.rapidapi.com/joke/random`;
+//  (has memes and jokes)    url: 'https://random-stuff-api.p.rapidapi.com/joke',
+// let joke = `https://random-stuff-api.p.rapidapi.com/joke/random`;
 
-  // fetch(joke, {
-  //   params: {exclude: "dirty, men, black, sex, sarcasm, insults, blonde, christian, easter, animal, rude, attitude, life,"},
-  //   headers: {
-  //     Authorization: "IcgIu9A1Tg9S",
-  //     "x-rapidapi-host": "random-stuff-api.p.rapidapi.com",
-  //     "x-rapidapi-key": "52449e7faemsh20985c2307930f2p15b4a7jsn86daa9a9788f",
-  //   },
-  // }).then(function (response) {
-  //   console.log(response);
-  //   return response.json()
-  // }).then(function(data){
-  //   console.log(data);
-  // },)
+// fetch(joke, {
+//   params: {exclude: "dirty, men, black, sex, sarcasm, insults, blonde, christian, easter, animal, rude, attitude, life,"},
+//   headers: {
+//     Authorization: "IcgIu9A1Tg9S",
+//     "x-rapidapi-host": "random-stuff-api.p.rapidapi.com",
+//     "x-rapidapi-key": "52449e7faemsh20985c2307930f2p15b4a7jsn86daa9a9788f",
+//   },
+// }).then(function (response) {
+//   console.log(response);
+//   return response.json()
+// }).then(function(data){
+//   console.log(data);
+// },)
 
-  // let joke = `https://random-stuff-api.p.rapidapi.com/joke/random`;
-  // fetch(url, options)
+// let joke = `https://random-stuff-api.p.rapidapi.com/joke/random`;
+// fetch(url, options)
 // 	.then(res => res.json())
 // 	.then(json => console.log(json))
 // 	.catch(err => console.error('error:' + err));
@@ -157,7 +199,7 @@ choice()
 
 // $.ajax(settings).done(function (response) {
 // 	console.log(response);
-// }); 
+// });
 
 // let meme = {
 // 	method: 'GET',
@@ -180,21 +222,21 @@ choice()
 //   cache: 'reload',
 // })
 
-  // let joke = `https://humor-jokes-and-memes.p.rapidapi.com/jokes/search?exclude-tags=nsfw&keywords=rocket&min-rating=7&include-tags=one_liner&number=3&max-length=200`;
-  // fetch(joke, {
-  //   params: {exclude: "nsfw, insults, jewish, religious, dark, racist, sexual, sexist,"},
-  //   headers: {
-  //     'X-RapidAPI-Key': '9caa14e070msh757ef190f56b5f8p137339jsn29e91c3f8df1',
-  //     'X-RapidAPI-Host': 'humor-jokes-and-memes.p.rapidapi.com'
-  //   },
-  // }).then(function (response) {
-  //   console.log(response);
-  //   return response.json()
-  // }).then(function(data){
-  //   console.log(data);
-  // },)
+// let joke = `https://humor-jokes-and-memes.p.rapidapi.com/jokes/search?exclude-tags=nsfw&keywords=rocket&min-rating=7&include-tags=one_liner&number=3&max-length=200`;
+// fetch(joke, {
+//   params: {exclude: "nsfw, insults, jewish, religious, dark, racist, sexual, sexist,"},
+//   headers: {
+//     'X-RapidAPI-Key': '9caa14e070msh757ef190f56b5f8p137339jsn29e91c3f8df1',
+//     'X-RapidAPI-Host': 'humor-jokes-and-memes.p.rapidapi.com'
+//   },
+// }).then(function (response) {
+//   console.log(response);
+//   return response.json()
+// }).then(function(data){
+//   console.log(data);
+// },)
 
-// DELETE BELOW????? 
+// DELETE BELOW?????
 // fetch(choice);{
 //   // When click on choice = funny meme or joke.
 //   $(choice).on("click", function () {
