30a5c2a06c05ce41a782a72a64abbbfb93cbf9cd
CF <cfreitas12345@gmail.com>
2023-02-07 21:25:36 -0700

    Set-up and organized likeDisArr.

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,4 +1,3 @@
-// KEEP BELOW, LINES 1-40 WORKS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 // Joke & meme buttons
 var jokeBtn = document.getElementById("jokeBtn");
 jokeBtn.addEventListener("click", jokeChoice);
@@ -39,8 +38,8 @@ function memeChoice() {
   let meme = `https://programming-memes-images.p.rapidapi.com/v1/memes`;
   fetch(meme, {
     headers: {
-      'X-RapidAPI-Key': '52449e7faemsh20985c2307930f2p15b4a7jsn86daa9a9788f',
-      'X-RapidAPI-Host': 'programming-memes-images.p.rapidapi.com'
+      "X-RapidAPI-Key": "52449e7faemsh20985c2307930f2p15b4a7jsn86daa9a9788f",
+      "X-RapidAPI-Host": "programming-memes-images.p.rapidapi.com",
     },
   })
     .then(function (response) {
@@ -51,13 +50,51 @@ function memeChoice() {
       console.log(data);
       let img = document.createElement("img");
 
-      img.src = data[Math.floor(Math.random() * data.length)].image
+      img.src = data[Math.floor(Math.random() * data.length)].image;
       console.log(img.meme);
       document.querySelector("#display").appendChild(img);
       document.querySelector(".getChoice").classList.add("hide");
       document.querySelector("#results").classList.remove("hide");
     });
 }
+// // like/dislike Btns clickEvents
+JSON.stringify(likeDisArray);
+{
+  var likeBtn = document.getElementById("likeBtn");
+  likeBtn.addEventListener("click", like);
+
+  var disBtn = document.getElementById("disBtn");
+  disBtn.addEventListener("click", dis);
+
+  function like() {
+    // Get like array from local storage joke/meme (*data attributes, then push, div ID display textContent/img)
+    localStorage.setItem("likeStore");
+    console.log(`likeStore`);
+  }
+
+  function dis() {
+    localStorage.setItem("disStore");
+    console.log(`disStore`);
+  }
+}
+
+// Dis/likeArray: When the user clicks like or dislike,
+// it's put into localStorage:
+$(document).ready(function () {
+  // Retrieves the like/dislike data and stores it into localStorage:
+  var likeDisArr = JSON.parse(localStorage.setItem("like"));
+  // likeDisArr = like/dislike array
+  if (likeDisArr !== null) {
+    Storage: var likeDisArr = JSON.parse(localStorage.setItem("dis"));
+    console.log(`likeDisArr localStorage`);
+  }
+});
+// window.location.reload()
+
+//   // Below stores the choice in localStorage
+//   localStorage.setItem("choice", JSON.stringify(like));
+//   console.log("choice", JSON.stringify(dis));
+// }
 
 // like/dislike localStorage
 //     /* Access image by id and change
@@ -67,51 +104,4 @@ function memeChoice() {
 
 //     document.getElementById('memeBtn')
 //             .style.display = "none";
-// }
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-// // like/dislike Btns clickEvents
-JSON.stringify(likeDisArray);{
-
-var likeBtn = document.getElementById("likeBtn");
-likeBtn.addEventListener("click", like);
-
-var disBtn = document.getElementById("disBtn");
-disBtn.addEventListener("click", dis);
-
-  function like() {
-// Get like array from local storage joke/meme (*data attributes, then push, div ID display textContent/img)
-       
-    localStorage.setItem("likeStore",    );
-    console.log(`likeStore`,   );
-  }
-
-  function dis() {
-    localStorage.setItem("disStore",      );     
-    console.log(`disStore`,      );
-  }
-}
-
-// When the user clicks like or dislike:
-$(document).ready(function () {
-  // Retrieves the like/dislike data and stores it into localStorage:
-  var likeDisArr = JSON.parse(localStorage.setItem("like"));
-  // likeDisArr = like/dislike array
-  if (likeDisArr !== null) {
-    Storage:
-  var likeDisArr = JSON.parse(localStorage.setItem("dis"));
-    console.log(`likeDisArr localStorage`);
-  }
-});
-// window.location.reload()
-
-
-
-
-
-
-//   // Below stores the choice in localStorage
-//   localStorage.setItem("choice", JSON.stringify(like));
-//   console.log("choice", JSON.stringify(dis));
 // }
